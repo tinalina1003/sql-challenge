1b6079e71f2083854805728a5bbec0c5de8ce708
Christina Leung <tinalina1003@gmail.com>
2023-10-18 20:53:39 -0400

    Added extra copies of the schema to test fixing foreign keys and constraints... import successful on schema4

--- EmployeeSQL/EmployeeSchema2.sql
+++ EmployeeSQL/EmployeeSchema2.sql
@@ -10,46 +10,40 @@ DROP TABLE IF EXISTS dept_manager;
 
 
 CREATE TABLE departments (
-    dept_no VARCHAR   NOT NULL,
-    dept_name VARCHAR   NOT NULL,
-	CONSTRAINT pk_departments PRIMARY KEY (dept_no)
+    dept_no VARCHAR PRIMARY KEY NOT NULL,
+    dept_name VARCHAR   NOT NULL
 );
 
 
 CREATE TABLE dept_emp (
-    emp_no INT   NOT NULL,
-    dept_no VARCHAR   NOT NULL,
-	CONSTRAINT pk_dept_emp PRIMARY KEY (emp_no)
+    emp_no INT PRIMARY KEY NOT NULL,
+    dept_no VARCHAR   NOT NULL
 );
 
 CREATE TABLE dept_manager (
-    dept_no VARCHAR   NOT NULL,
-    emp_no INT   NOT NULL,
+    dept_no VARCHAR  NOT NULL,
+    emp_no INT  NOT NULL,
 	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE employees (
-    emp_no INT   NOT NULL,
+    emp_no INT  PRIMARY KEY NOT NULL,
     emp_title VARCHAR   NOT NULL,
     birth_date VARCHAR   NOT NULL,
     first_name VARCHAR   NOT NULL,
     last_name VARCHAR   NOT NULL,
     sex VARCHAR   NOT NULL,
-    hire_date VARCHAR   NOT NULL,
-	CONSTRAINT pk_employees PRIMARY KEY (emp_no)
+    hire_date VARCHAR   NOT NULL
 );
 
 CREATE TABLE salaries (
-    emp_no INT   NOT NULL,
-    salary INT   NOT NULL,
-	CONSTRAINT pk_salaries PRIMARY KEY (emp_no)
+    emp_no INT PRIMARY KEY  NOT NULL,
+    salary INT   NOT NULL
 );
 
 CREATE TABLE titles (
-    title_id VARCHAR   NOT NULL,
-    title VARCHAR   NOT NULL,
-	CONSTRAINT pk_titles PRIMARY KEY (title_id),
-	CONSTRAINT uk_titles_title UNIQUE (title)
+    title_id VARCHAR PRIMARY KEY  NOT NULL,
+    title VARCHAR   NOT NULL
 );
 
 ALTER TABLE departments ADD CONSTRAINT fk_departments_dept_no FOREIGN KEY(dept_no)
